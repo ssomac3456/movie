a6891c9b8f50092f4ef26e924ce052a7cce526f8
ssomac3456 <cbrcbr219@naver.com>
2026-07-14 12:23:50 +0900

    first

---
+++ README.md
@@ -0,0 +1,11 @@
+# 영화 리뷰앱 작업리스트
+
+- [ ] Router 설정
+- [ ] 각 컴포넌트, 폴더 구성(IA)
+- [ ] api 설정
+- [ ] Header, footer, Loading, 404
+- [ ] 웹폰트
+- [ ] 각 페이지 ui + 반응형 작업
+- [ ] Helmet
+- [ ] Page Scroll Event
+- [ ] Deploy
---
+++ src/Router.jsx
@@ -0,0 +1,18 @@
+import { HashRouter, Route, Routes } from "react-router-dom";
+import Movie from "./pages/movie/movie";
+import Home from "./pages/home/Home";
+import Search from "./pages/search/Searcl";
+import ErrorPage from "./pages/ErrorPage";
+
+export default function Router() {
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movie/:id" element={<Movie />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/*" element={<ErrorPage />} />
+      </Routes>
+    </HashRouter>
+  );
+}
