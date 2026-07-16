72e0e751be5288aa0a504bdb0db909fe58089713
ssomac3456 <cbrcbr219@naver.com>
2026-07-16 13:45:42 +0900

    7.16.2

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -2,7 +2,7 @@ export default function ErrorPage() {
   return (
     <div>
       <div className="flex justify-center items-center">
-        <p>MOVIE</p>
+        <p className="text-white">MOVIE</p>
         <h1>404</h1>
         <h2>페이지를 찾을 수 없습니다.</h2>
         <p>요청한 페이지가 존재하지 않거나, 이동되었을 수 있습니다.</p>
